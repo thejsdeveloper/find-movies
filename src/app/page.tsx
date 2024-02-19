53098faafb64d6375a211ed51ddb57e5cc9040ae
vikas yadav <thejsdeveloper@gmail.com>
2024-02-19 22:04:00 +0530

    starting template

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      This is movie app
+      This is starting template for Find Movies
     </main>
   );
 }
